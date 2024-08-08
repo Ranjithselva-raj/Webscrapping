370c1080d3c3e04431dbe8c7b06e1d52dead8c2b
Ranjithselva-raj <ranjee1691@gmail.com>
2024-08-08 19:44:07 +0100

    initial commit

---
+++ 1.Single_category_scrape/main.py
@@ -0,0 +1,26 @@
+import httpx
+from selectolax.parser import HTMLParser
+
+url = "https://www.rei.com/c/footwear/f/scd-deals"
+#footwear #camping-and-hiking
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"}
+
+resp = httpx.get(url, headers=headers)
+
+html = HTMLParser(resp.text)
+#print(html.css_first("title").text())
+
+def extract_text(html, sel):
+    try:
+        return html.css_first(sel).text()
+    except AttributeError:
+        return None 
+
+products = html.css("li.VcGDfKKy_dvNbxUqm29K")
+
+for product in products:
+    item={
+        "Name": extract_text(product,".Xpx0MUGhB7jSm5UvK2EY"),
+        "Price": extract_text(product,"span[data-ui=sale-price]")
+    }
+    print(item)
