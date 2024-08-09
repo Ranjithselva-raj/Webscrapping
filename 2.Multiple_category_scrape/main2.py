3100e0898d266cca569f3413e6a316b03baaea6a
Ranjithselva-raj <ranjee1691@gmail.com>
2024-08-09 06:38:26 +0100

    comments added

--- 2.Multiple_category_scrape/main2.py
+++ 2.Multiple_category_scrape/main2.py
@@ -66,27 +66,33 @@ for category in Category_list:
             url = f"https://www.rei.com/c/{category}/f/scd-deals"
         else:
             url = f"https://www.rei.com/c/{category}/f/scd-deals?page={page}"
-    
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"}
 
+        # headers are mimic the browser headers from the browser used to access the site 
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"}
+        # httpx is same as requests but with some extra features like http2 and proxies 
         resp = httpx.get(url, headers=headers)
+        #html parser is used to parse the html from the response text 
         html = HTMLParser(resp.text)
-        #print(html.css_first("title").text())
+            #print(html.css_first("title").text())
+        #html.css is used to select the elements from the html based on the css selector
         products = html.css("li.VcGDfKKy_dvNbxUqm29K")
 
+        #if the length of products is 0 then break the loop
         if len(products) == 0:
             break
-
+        #for each product in products object extract the name, price and image
         for product in products:
             item={
                 "Name": extract_text(product,".Xpx0MUGhB7jSm5UvK2EY"),
                 "Price": extract_text(product,"span[data-ui=sale-price]"),
                 "Image": extract_attribute(product,"img", "src")
             }
+            # json.dumps is used to convert the item dictionary to a string
             string_item = json.dumps(item)
             content.append(string_item)
         page += 1
-    #write_file(category+".txt",content)
+    # write each category content to a json or txt file seperately
+    #(.json or txt )
     write_file(category+".json",content)
-
+    # print the length of content and the name of the category when each category is processed
     print(f"{len(content)} items written to {category}.json")
